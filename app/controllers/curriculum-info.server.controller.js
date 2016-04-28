bd0f5beca689aeb8e3e18246ffb9dc23be3f7ba0
Salman Syed <salman_syed55@hotmail.com>
2016-04-27 23:05:24 -0400

    minor cleanup

--- app/controllers/curriculum-info.server.controller.js
+++ app/controllers/curriculum-info.server.controller.js
@@ -9,9 +9,9 @@ var mongoose = require('mongoose'),
     request = require('request');
 
 /* constants */
-var CURRICULUM_INFO_ENDPOINT = 'https://elastic1.asn.desire2learn.com/api/1/search';
-var ASN_API_KEY = null;
-var BQ_PREFIX = "?bq=(and+jurisdiction:'Ontario'+publication_status:'Published'+has_child:'false'+type:'Statement'";
+var CURRICULUM_INFO_ENDPOINT = 'https://elastic1.asn.desire2learn.com/api/1/search',
+    ASN_API_KEY = null,
+    BQ_PREFIX = "?bq=(and+jurisdiction:'Ontario'+publication_status:'Published'+has_child:'false'+type:'Statement'";
 
 exports.getCurriculumData = function(req, res) {
   // curriculum info retrieval data
@@ -23,14 +23,14 @@ exports.getCurriculumData = function(req, res) {
   };
 
   // build bq query param and url
-  var bq = BQ_PREFIX + "+education_level:'" + req.query.grade + "'+subject:'" + req.query.subject + "')&";
-  var url = CURRICULUM_INFO_ENDPOINT + bq + querystring.stringify(data);
+  var bq = BQ_PREFIX + "+education_level:'" + req.query.grade + "'+subject:'" + req.query.subject + "')&",
+      url = CURRICULUM_INFO_ENDPOINT + bq + querystring.stringify(data);
 
   // retrieve curriculum info for grade and subject
   request(url, function (error, response, body) {
     if (! error && response.statusCode === 200) {
-      var currRawData = JSON.parse(body).hits.hit;
-      var currList = [];
+      var currRawData = JSON.parse(body).hits.hit,
+          currList = [];
       for(var i = 0; i < currRawData.length; i++) {
         var data = currRawData[i].data;
 
@@ -53,14 +53,14 @@ exports.getGrades = function(req, res) {
   };
 
   // build bq query param and url
-  var bq = BQ_PREFIX + ')&';
-  var url = CURRICULUM_INFO_ENDPOINT + bq + querystring.stringify(data);
+  var bq = BQ_PREFIX + ')&',
+      url = CURRICULUM_INFO_ENDPOINT + bq + querystring.stringify(data);
 
   // retrieve list of education levels
   request(url, function (error, response, body) {
     if (! error && response.statusCode === 200) {
-      var constraints = JSON.parse(body).facets.fct_education_level.constraints;
-      var grades = [];
+      var constraints = JSON.parse(body).facets.fct_education_level.constraints,
+          grades = [];
       for (var i = 0; i < constraints.length; i++) {
         grades.push(constraints[i].value);
       }
@@ -79,15 +79,15 @@ exports.getSubjects = function(req, res) {
   };
 
   // build bq query param and url
-  var bq = BQ_PREFIX + "+education_level:'" + req.query.grade + "')&";
-  var url = CURRICULUM_INFO_ENDPOINT + bq + querystring.stringify(data);
+  var bq = BQ_PREFIX + "+education_level:'" + req.query.grade + "')&",
+      url = CURRICULUM_INFO_ENDPOINT + bq + querystring.stringify(data);
 
   // retrieve list of subjects
   request(url, function (error, response, body) {
     if (! error && response.statusCode === 200) {
-      var constraints = JSON.parse(body).facets.fct_subject.constraints;
-      var len = constraints.length;
-      var subjects = new Array(len);
+      var constraints = JSON.parse(body).facets.fct_subject.constraints,
+          len = constraints.length,
+          subjects = new Array(len);
       for (var i = 0; i < len; i++) {
         subjects[i] = constraints[i].value;
       }
