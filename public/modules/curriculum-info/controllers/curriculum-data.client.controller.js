81d4ffd0d1ece7509ebd71a4c62f4781a4d3512d
Salman Syed <salman_syed55@hotmail.com>
2016-05-10 13:27:48 -0400

    UI for add comment modal

--- public/modules/curriculum-info/controllers/curriculum-data.client.controller.js
+++ public/modules/curriculum-info/controllers/curriculum-data.client.controller.js
@@ -1,12 +1,48 @@
 'use strict';
 
-angular.module('curriculum-info').controller('CurriculumDataController', ['$scope', '$location',
-	function($scope, $location) {
+angular.module('curriculum-info').controller('CurriculumDataController', ['$scope', '$location', '$uibModal',
+	function($scope, $location, $uibModal) {
 
-		// get curriculum topic details and comments
+		/* TODO: get curriculum topic details and comments */
 		$scope.getCurriculumTopicData = function() {
 			var id = $location.search().id;
-
 		};
+
+		/* displays add comment modal */
+		$scope.addComment = function() {
+			$uibModal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'commentModal.html',
+				controller: 'CommentModalController'
+			}).result.then(function(response) {
+				// TODO: handle comment data
+			});
+		};
+	}
+]);
+
+angular.module('curriculum-info').controller('CommentModalController', ['$scope', '$uibModalInstance',
+	function ($scope, $uibModalInstance) {
+
+		/* constants */
+		$scope.RATINGS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+		/* returns modal data */
+		$scope.add = function() {
+			$uibModalInstance.close({ rating: $scope.rating,
+																difficultAreas: $scope.difficultAreas,
+																techniques: $scope.techniques,
+																date: new Date() });
+		};
+
+		/* closes modal */
+		$scope.cancel = function() {
+			$uibModalInstance.dismiss();
+		};
+
+		/* clears all modal fields */
+		$scope.reset = function() {
+			$scope.rating = $scope.difficultAreas = $scope.techniques = '';
+		}
 	}
 ]);
